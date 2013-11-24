e76f3e777d6d7f93f531f11472f434f29d6a9525
jlagneau <jlagneau@student.42.fr>
2013-11-24 15:44:58 +0100

    toto

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:39:15 by jlagneau          #+#    #+#             */
-/*   Updated: 2013/11/24 10:17:19 by jlagneau         ###   ########.fr       */
+/*   Updated: 2013/11/24 15:44:09 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	ft_putstr("ft_strnstr");
 	src = (char *) s1;
 	len = ft_strlen(s2);
-	while (*src || n--)
+	if (*s2 == '\0' || s2 == NULL)
+		return (src);
+	while (*src && n--)
 	{
 		if (ft_memcmp(src, s2, len) != 0)
 			return (src);
