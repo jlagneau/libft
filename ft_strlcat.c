2e83f9ac0befd8815ee7da069b661e5f7757d48a
jlagneau <jlagneau@student.42.fr>
2013-11-25 18:56:41 +0100

    toto

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:31:17 by jlagneau          #+#    #+#             */
-/*   Updated: 2013/11/25 15:48:54 by jlagneau         ###   ########.fr       */
+/*   Updated: 2013/11/25 18:56:00 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,7 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
-	size_t		len_dst;
-	size_t		len_src;
-	char		*tmp;
-
-	if (dst == 0 || src == 0)
+	if (!dst || !src)
 		return (0);
-	tmp = dst;
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
-	n -= len_dst;
-	if (n <= 0)
-		return (len_dst + len_src);
-	tmp += len_dst;
-	ft_memcpy(tmp, src, n);
-	return (len_dst + len_src);
+	return (n);
 }
