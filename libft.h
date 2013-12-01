f43fd88756244d2f38cbd25c89cf325acd234761
jlagneau <jlagneau@student.42.fr>
2013-12-01 21:42:06 +0100

    toto

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:29:44 by jlagneau          #+#    #+#             */
-/*   Updated: 2013/12/01 21:10:46 by jlagneau         ###   ########.fr       */
+/*   Updated: 2013/12/01 21:18:39 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -79,6 +79,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new);
 /* PERSONNAL FUNCTION */
 int		ft_isspace(int c);
 void	ft_str_array_del(char **s);
