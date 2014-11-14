abe82fc16e8e99a240b9fcb000993a6109a7013d
jlagneau <jlagneau@student.42.fr>
2014-11-15 00:57:38 +0100

    Fix norm on t_bool

--- include/ft_def.h
+++ include/ft_def.h
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:29:44 by jlagneau          #+#    #+#             */
-/*   Updated: 2014/05/13 13:10:51 by jlagneau         ###   ########.fr       */
+/*   Updated: 2014/11/06 22:53:41 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 
 typedef		enum				e_bool
 {
-								TRUE,
-								FALSE
+								TRUE, FALSE
 }								t_bool;
 
 typedef		unsigned long		t_size;
