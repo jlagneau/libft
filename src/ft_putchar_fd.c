88db190912a719d890a54f51f7fa0be609ae3f1d
jlagneau <jlagneau@student.42.fr>
2016-01-03 02:04:57 +0100

    Check if there was an error using write

--- src/ft_putchar_fd.c
+++ src/ft_putchar_fd.c
@@ -11,8 +11,10 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
-	write(fd, &c, 1);
+	if (write(fd, &c, 1) == -1)
+		perror(__FILE__);
 }
