b7ea3677c02b302d423da1aeb732a53f8d40149f
jlagneau <jlagneau@student.42.fr>
2016-04-16 05:34:55 +0200

    Limit syscalls for ft_putstr

--- src/ft_putstr_fd.c
+++ src/ft_putstr_fd.c
@@ -6,14 +6,14 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:32:44 by jlagneau          #+#    #+#             */
-/*   Updated: 2013/12/01 17:25:02 by jlagneau         ###   ########.fr       */
+/*   Updated: 2016/04/16 05:33:57 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ft_io.h>
+#include <unistd.h>
+#include <ft_str.h>
 
 void	ft_putstr_fd(char const *s, int fd)
 {
-	while (*s)
-		ft_putchar_fd(*s++, fd);
+	write(fd, s, ft_strlen(s));
 }
