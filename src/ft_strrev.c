638edb76edacb2746d0f6ecc2f76e7294e7261a9
jlagneau <jlagneau@student.42.fr>
2016-09-07 12:09:50 +0200

    Fix ft_strrev

--- src/ft_strrev.c
+++ src/ft_strrev.c
@@ -6,27 +6,31 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 11:15:34 by jlagneau          #+#    #+#             */
-/*   Updated: 2016/09/07 12:00:28 by jlagneau         ###   ########.fr       */
+/*   Updated: 2016/09/07 12:09:13 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_str.h>
 
-char					*ft_strrev(char *s)
+void	ft_swap(char *c1, char *c2)
+{
+	*c1 ^= *c2;
+	*c2 ^= *c1;
+	*c1 ^= *c2;
+}
+
+char	*ft_strrev(char *s)
 {
 	t_size	len;
 	t_size	start;
 	t_size	end;
-	char	tmp;
 
 	len = ft_strlen(s);
 	start = 0;
 	end = len - 1;
 	while (start < end)
 	{
-		tmp = *(s + start);
-		*(s + start) = *(s + end);
-		*(s + end) = tmp;
+		ft_swap((s + start), (s + end));
 		start++;
 		end--;
 	}
