ae676743ff1173eb4ba5c382e1917e6d9b917678
jlagneau <jlagneau@student.42.fr>
2016-09-07 12:57:37 +0200

    Factorize ft_strrev

--- src/ft_strrev.c
+++ src/ft_strrev.c
@@ -6,33 +6,29 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/07 11:15:34 by jlagneau          #+#    #+#             */
-/*   Updated: 2016/09/07 12:09:13 by jlagneau         ###   ########.fr       */
+/*   Updated: 2016/09/07 12:57:02 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_str.h>
 
-void	ft_swap(char *c1, char *c2)
+char    *ft_strrev(char *s)
 {
-	*c1 ^= *c2;
-	*c2 ^= *c1;
-	*c1 ^= *c2;
-}
+    char    *start;
+    char    *end;
+    char    tmp;
 
-char	*ft_strrev(char *s)
-{
-	t_size	len;
-	t_size	start;
-	t_size	end;
-
-	len = ft_strlen(s);
-	start = 0;
-	end = len - 1;
-	while (start < end)
-	{
-		ft_swap((s + start), (s + end));
-		start++;
-		end--;
-	}
-	return (s);
+	if (*s == '\0')
+		return (s);
+    start = s;
+    end = start + ft_strlen(s) - 1;
+    while (start < end)
+    {
+        tmp = *start;
+        *start = *end;
+        *end = tmp;
+        start++;
+        end--;
+    }
+    return (s);
 }
