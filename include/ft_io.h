e13009d8dd71c28fbcf525dd1839a778df521604
jlagneau <jlagneau@student.42.fr>
2016-09-08 11:59:08 +0200

    Add new functions * ft_putnbr_long * ft_putnbr_long_fd * ft_putnbr_base * ft_putnbr_base_fd

--- include/ft_io.h
+++ include/ft_io.h
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:29:44 by jlagneau          #+#    #+#             */
-/*   Updated: 2013/12/02 19:20:31 by jlagneau         ###   ########.fr       */
+/*   Updated: 2016/09/08 11:29:39 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,9 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr_long(long n);
+void	ft_putnbr_long_fd(long n, int fd);
+void	ft_putnbr_base(unsigned long n, unsigned int base);
+void	ft_putnbr_base_fd(unsigned long n, unsigned int base, int fd);
 
 #endif
