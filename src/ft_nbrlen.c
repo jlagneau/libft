3575cdf8bee197f8e9f49bff860ce230c1f1e657
jlagneau <jlagneau@student.42.fr>
2016-09-11 17:47:13 +0200

    Add ft_nbrlen function

---
+++ src/ft_nbrlen.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nbrlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/11 16:34:03 by jlagneau          #+#    #+#             */
+/*   Updated: 2016/09/11 16:42:50 by jlagneau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_def.h>
+
+size_t	ft_nbrlen(int n)
+{
+	const t_bool	is_negative = (n < 0) ? TRUE : FALSE;
+	size_t			size;
+
+	size = 0;
+	if (n == 0)
+		return (1);
+	while (n)
+	{
+		n /= 10;
+		size++;
+	}
+	return ((is_negative == TRUE) ? size + 1 : size);
+}
