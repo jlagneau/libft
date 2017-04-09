07c4321335de0899bf6036aa6fe7e43c6c9a8d79
jlagneau <jlagneau@student.42.fr>
2017-04-09 10:12:25 +0200

    Use STDERR define

--- src/ft_putstr_fd.c
+++ src/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 08:32:44 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/08 12:42:13 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/04/08 20:38:28 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	ft_putstr_fd(char const *s, int fd)
 		nptr = backtrace(tmp, BUFF_SIZE);
 		while (i < nptr)
 		{
-			ft_putendl_fd(tmp[i], 2);
+			ft_putendl_fd(tmp[i], STDERR);
 			i++;
 		}
 		exit(-1);
