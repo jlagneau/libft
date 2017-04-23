e52041afde37bb15183b620fe2da45084ad260b4
jlagneau <jlagneau@student.42.fr>
2017-04-23 12:20:34 +0200

    Add ft_wstrncpy ft_wstrnew

--- src/ft_wstrncpy.c
+++ src/ft_wstrncpy.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_wchar.h                                         :+:      :+:    :+:   */
+/*   ft_wstrncpy.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/23 02:31:26 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/23 12:13:04 by jlagneau         ###   ########.fr       */
+/*   Created: 2017/04/23 12:00:44 by jlagneau          #+#    #+#             */
+/*   Updated: 2017/04/23 12:03:17 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_WCHAR_H
-# define FT_WCHAR_H
-# include <unistd.h>
-# include <wchar.h>
+#include <libft.h>
 
-ssize_t		ft_wcharlen(const wchar_t c);
-ssize_t		ft_wstrlen(const wchar_t *s);
-size_t		ft_wstrwlen(const wchar_t *s);
-char		*ft_winttostr(const wint_t c);
-char		*ft_wchartostr(const wchar_t *s);
-wchar_t		*ft_wstrncpy(wchar_t *s1, const wchar_t *s2, size_t n);
-wchar_t		*ft_wstrnew(size_t size);
+wchar_t		*ft_wstrncpy(wchar_t *s1, const wchar_t *s2, size_t n)
+{
+	size_t	i;
 
-#endif
+	i = 0;
+	ft_bzero(s1, n * sizeof (wchar_t));
+	while (s2[i] && i < n)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	return (s1);
+}
--- src/ft_wstrnew.c
+++ src/ft_wstrnew.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_wchar.h                                         :+:      :+:    :+:   */
+/*   ft_wstrnew.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/23 02:31:26 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/23 12:13:04 by jlagneau         ###   ########.fr       */
+/*   Created: 2017/04/23 12:10:25 by jlagneau          #+#    #+#             */
+/*   Updated: 2017/04/23 12:11:47 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_WCHAR_H
-# define FT_WCHAR_H
-# include <unistd.h>
-# include <wchar.h>
+#include <libft.h>
 
-ssize_t		ft_wcharlen(const wchar_t c);
-ssize_t		ft_wstrlen(const wchar_t *s);
-size_t		ft_wstrwlen(const wchar_t *s);
-char		*ft_winttostr(const wint_t c);
-char		*ft_wchartostr(const wchar_t *s);
-wchar_t		*ft_wstrncpy(wchar_t *s1, const wchar_t *s2, size_t n);
-wchar_t		*ft_wstrnew(size_t size);
+wchar_t		*ft_wstrnew(size_t size)
+{
+	wchar_t		*tmp;
 
-#endif
+	tmp = NULL;
+	if (!(tmp) = (wchar_t*)ft_memalloc(size * sizeof (wchar_t) + 1))
+		return (NULL);
+	return (tmp);
+}
