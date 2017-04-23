9c42ab270c38394e141b8442d0eefceb83ec7248
jlagneau <jlagneau@student.42.fr>
2017-04-23 13:38:47 +0200

    Fix ft_wstrnew

--- src/ft_wstrnew.c
+++ src/ft_wstrnew.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/23 12:10:25 by jlagneau          #+#    #+#             */
-/*   Updated: 2017/04/23 12:11:47 by jlagneau         ###   ########.fr       */
+/*   Updated: 2017/04/23 13:38:27 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ wchar_t		*ft_wstrnew(size_t size)
 	wchar_t		*tmp;
 
 	tmp = NULL;
-	if (!(tmp) = (wchar_t*)ft_memalloc(size * sizeof (wchar_t) + 1))
+	if (!(tmp = (wchar_t*)ft_memalloc(size * sizeof (wchar_t) + 1)))
 		return (NULL);
 	return (tmp);
 }
